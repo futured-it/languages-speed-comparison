8d52ea94979dbdd884ba65a8052d2a6ace62f92e
futured_it <73485120+futured-it@users.noreply.github.com>
2023-01-05 19:56:02 +0500

    chore: Syntax fix

--- src/fibonacci/cpp/fibonacci.cpp
+++ src/fibonacci/cpp/fibonacci.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
   
-void fib(long n) {
+void fib(long n)
+{
     long a = 0, b = 1;
-    while (a < n) {
+    while (a < n) 
+    {
         cout << a << ' ';
         swap(a,b);
         b = a+b;
     }
+    cout << endl;
 }
 
 int main(int argc, char *argv[])
 {
     fib(stol(argv[1]));
-    cout << endl;
     return 0;
 }
