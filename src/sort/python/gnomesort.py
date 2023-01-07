99222814f9af05b93e2edfd6c693691151d34686
futured_it <73485120+futured-it@users.noreply.github.com>
2023-01-07 16:35:30 +0500

    feat: GnomeSort algorithm added

---
+++ src/sort/python/gnomesort.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+def gnomeSort(list):
+    pos = 0
+    while pos < len(list):
+        if (pos == 0) or (list[pos] >= list[pos-1]):
+            pos += 1
+        else:
+            list[pos], list[pos-1] = list[pos-1], list[pos]
+            pos -= 1
+        print(*list)
+    return list
+
+print(*gnomeSort([ int(x) for x in argv[1:] ]))
