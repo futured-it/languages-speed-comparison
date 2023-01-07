982b85b0f8ed42b8779c935e770005f828a93b5b
futured_it <73485120+futured-it@users.noreply.github.com>
2023-01-07 17:43:21 +0500

    chore: BubbleSort ported to JS

---
+++ src/sort/js/bubblesort.js
@@ -0,0 +1,20 @@
+function bubbleSort(list) {
+    let n = list.length
+    let sorted = false
+    while (!sorted) {
+        sorted = true
+        for (let i = 0; i < n-1; i++) {
+            if (list[i] > list[i+1]) {
+                [ list[i], list[i+1] ] = [ list[i+1], list[i] ]
+                sorted = false
+            }
+        }
+    }
+    return list
+}
+
+console.log(
+    bubbleSort(process.argv.slice(2).map(function (x) { 
+        return parseInt(x, 10); 
+    })).join(" ")
+)
