e31f58786950fc7087658e5e493e66fc9885a1a1
futured_it <73485120+futured-it@users.noreply.github.com>
2023-02-18 00:06:06 +0500

    chore: SelectionSort JS fixes

--- src/sort/js/selectionsort.js
+++ src/sort/js/selectionsort.js
@@ -1,15 +1,13 @@
 function selectionSort(list) {
-    let n = list.length
-    console.log(n)
+    let n = list.length;
     for (let i = 1; i < n; i++) {
         let jMin = i
         for (let j = i+1; j < n; j++) {
             if (list[j] < list[jMin]) jMin = j
-            //console.log(list)
         }
         if (jMin !== i) [ list[i], list[jMin] ] = [ list[jMin], list[i] ]
-    }
-    return list
+    };
+    return list;
 }
 
 console.log(
